3fe1429b0464c6a44dd41670991c0b201318f1b3
mchardex <bukunmiadebisi@gmail.com>
2020-04-24 20:39:41 +0100

    feature(landing-page): update responsiveness
    - update landing page responsiveness

--- src/Pages/LandingPage/__tests__/LandingPage.test.tsx
+++ src/Pages/LandingPage/__tests__/LandingPage.test.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import {render} from '@testing-library/react';
+import {BrowserRouter as Route} from 'react-router-dom';
 import {LandingPage} from '..';
 
-const props = {
-  isLoading: false,
-  message: '',
-  error: '',
-  reduxRequest: jest.fn(),
-};
-
 describe('<LandingPage />', () => {
-  test('renders learn react link', () => {
-    const {getByText} = render(<LandingPage {...props} />);
-    const linkElement = getByText(/We are getting started soon!!!/i);
-    expect(linkElement).toBeInTheDocument();
+  test('it should match with previous snapshot', () => {
+    const {container} = render(
+      <Route>
+        <LandingPage />
+      </Route>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
